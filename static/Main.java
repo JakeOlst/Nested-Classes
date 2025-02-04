d3c88d9384af63580b03052883c652ab75b45da9
JakeOlst <jstainthorpe123@gmail.com>
2025-02-04 19:27:41 +0000

    Added class for VendingMachine and nested static classes.  implemented functionality

---
+++ static/Main.java
@@ -0,0 +1,29 @@
+class VendingMachine {
+
+  public static class ChocolateBar {
+    public String getName() {
+      return "ChocoBlo";
+    }
+
+    public double getPrice() {
+      return 1.30;
+    }
+  }
+
+  public static class BagOfChips {
+    public String getName() {
+      return "Sea Salt Chips";
+    }
+
+    public double getPrice() {
+      return 1.60;
+    }
+  }
+  
+}
+
+public class Main {
+  public static void main(String[] args){
+    
+  }
+}
